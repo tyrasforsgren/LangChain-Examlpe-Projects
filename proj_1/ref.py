b6754f0c3714179b9fe71c8a0629923227c5a6ca
Tyra Forsgren <forsgren.tyra@gmail.com>
2024-02-14 17:19:31 +0100

    All module method finished, reformatted script.

--- proj_1/ref.py
+++ proj_1/ref.py
@@ -1,90 +1,72 @@
-import openai
+
 import os
 from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
-from langchain.output_parsers import CommaSeparatedListOutputParser
-
-from langchain.schema import HumanMessage,SystemMessage
-from langchain.prompts import (ChatPromptTemplate , 
-                               SystemMessagePromptTemplate,
-                               HumanMessagePromptTemplate,
-                               AIMessagePromptTemplate,
-                               
-                               )
+from langchain.prompts import ChatPromptTemplate,HumanMessagePromptTemplate
 
 # Set your OpenAI API key
-
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
 
-class MarketingGenerator():
-    def __init__(self):
-        self.llm = ChatOpenAI(model_name="gpt-3.5-turbo",
+def generate_marketing(system_template, product_description): # TODO : Make the warnings go away ? 
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo",
                                openai_api_key=API_KEY,
                                temperature=1,
                                max_tokens=300)
+    # Create human prompt with proper template
+    human_template = "{product_description_temp}"
+    human_prompt = HumanMessagePromptTemplate.from_template(human_template)
+    # traanslate prompt
+    chat_prompt = ChatPromptTemplate.from_messages([
+        system_template,
+        human_prompt])
+    final_prompt = chat_prompt.format_prompt(
+        product_description_temp = product_description)
+    # Generate
+    result = llm.invoke(final_prompt)
+    return result.content
 
-    def generate_marketing_content(self,product_description,pros):
-        """
-        Generate persuasive marketing content using CHATGPT3.5 and DALL-E for LinkedIn.
+class MarketingGenerator():
 
-        Args:
-        - description (str): Brief description of the course or product.
-
-        Returns:
-        - str: Persuasive marketing content.
-        """
-        
-        # create System prompt template
-        system_template = "You are a marketing expert and will generate creative presuasive \
-                        marketing about a product. You highlight the {pros_temp}."
-        human_template = "{product_description_temp}"
-        human_prompt = HumanMessagePromptTemplate.from_template(human_template)
-        chat_prompt = ChatPromptTemplate.from_messages([
-            system_template,
-            human_prompt])
-        final_prompt = chat_prompt.format_prompt(
-            pros_temp = pros,
-            product_description_temp = product_description)
-
-        result = self.llm.invoke(final_prompt)
-        return result.content
-
-    def generate_catchy_titles(self,product_description):
-        """
-        Generate catchy titles for IT, cloud solutions, architecture, and ML/AI courses.
-
-        Args:
-        - description (str): Brief description of the course.
-
-        Returns:
-        - str: Catchy title.
-        """
+    def generate_titles(self, description):
         system_template = "You are a marketing expert and will generate creative presuasive \
                         titles for a product. Come up with several options."
-        human_template = "{product_description_temp}"
-        human_prompt = HumanMessagePromptTemplate.from_template(human_template)
-        
-        chat_prompt = ChatPromptTemplate.from_messages([
-            system_template,
-            human_prompt])
-        final_prompt = chat_prompt.format_prompt(
-            product_description_temp = product_description)
+        return generate_marketing(system_template, description)
 
-        result = self.llm.invoke(final_prompt)
-        return result.content
+    def generate_marketing_text(self, description):
+        system_template = "You are a marketing expert and will generate creative presuasive \
+                        marketing about a product."
+        return generate_marketing(system_template, description)
 
-# # Example usage:
-# course_description = "Learn how to build scalable cloud solutions using AWS services."
-# marketing_content = generate_marketing_content(course_description)
-# catchy_title = generate_catchy_titles(course_description)
+    def generate_advanced_course_outline(self, description):
+        system_template = "You are a teacher in the following subjects : IT, cloud soulutions, \
+                        system architecture, ML and AI. You will generate an ADVANCED course \
+                        plan/outline FOR PROFESSIONALS based on the description of a course."
+        return generate_marketing(system_template, description)
+    
+    def generate_beginner_course_outline(self, description):
+        system_template = "You are a teacher in the following subjects : IT, cloud soulutions, \
+                        system architecture, ML and AI. You will generate a SIMPLE course \
+                        plan/outline FOR BEGINNERS based on the description of a course."
+        return generate_marketing(system_template, description)
 
-# print("Generated Marketing Content:\n", marketing_content)
-# print("\nGenerated Catchy Title:\n", catchy_title)
+    def generate_coding_exersizes(self, subject, difficulty=None):
+        system_template = "You are a teacher in programming in Python. Generate a coding exersize for \
+                        the given subject. If a difficulty is given, match that in the complexity of \
+                        the problem"
+        if difficulty:
+            subject += f'Make this problem at {difficulty} difficulty'
+        return generate_marketing(system_template, subject)
+
+marketing_aid = MarketingGenerator()
 
 description = '''A course that teaches juniors how to code python'''
-pros = ['Free','Long Distance','Short']
-marketing_aid = MarketingGenerator()
-# print(marketing_aid.generate_marketing_content(description,pros))
-# print(marketing_aid.generate_catchy_titles(description))
+course_description = '''A course teaching data analasys'''
+subject = '''Pandas module'''
+
+# print(marketing_aid.generate_marketing_text(description))
+# print(marketing_aid.generate_titles(description))
+# print(marketing_aid.generate_beginner_course_outline(course_description))
+# print(marketing_aid.generate_advanced_course_outline(course_description))
+# print(marketing_aid.generate_coding_exersizes(subject))#,difficulty='extreme'))
