61e583f18612bc13efae57862fa7ffa58c8ddd97
Tyra Forsgren <forsgren.tyra@gmail.com>
2024-02-15 18:02:22 +0100

    Commit 8 : TextToSpeech module restructured. More staticmethods.

--- proj_2/TextToSpeechFromImage.py
+++ proj_2/TextToSpeechFromImage.py
@@ -1,33 +1,29 @@
+
 import cv2
 import pytesseract
 import matplotlib.pyplot as plt
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
+from PIL.Image import fromarray
 
-class MedicinePackageAnalyzer:
-    def __init__(self, image_path, template_coordinates):
-        self.image_path = image_path
-        self.full_image = self.read_image(self.image_path)
-        self.template_coordinates = template_coordinates
-    
-    def extract_text_from_image(self):
-        # Obtain the text and numbers from each text/numeric area in the template
-        self.extracted_text = {}
-        for area, image in self.sub_images.items():
-            self.extracted_text[area] = pytesseract.image_to_string(image)
+
+class TextToSpeech:
+    def __init__(self, image): # TODO is this needed?
+        # self.image_path = image_path
+        # self.image = image #BasicImageHandeling.read_image(image_path)
+        pass
+
+    @staticmethod
+    def extract_text_from_image(image): # TODO: Working but needs prior attention
+        # Use pytesseract to extract text from the image
+        processed_image = fromarray(image)
+        extracted_text = pytesseract.image_to_string(processed_image)
+
+        return extracted_text
     
     def find_title_and_info(self):
         # Find the title, main color, and any special info in the package picture
         pass
-
-    @staticmethod
-    def read_image(image_path):
-        return cv2.imread(image_path)
-    
-    @staticmethod
-    def crop_image(image,coords):
-        x, y, w, h = coords
-        return image[y:y+h, x:x+w]
 
     @staticmethod
     def generate_summary_text():
@@ -41,11 +37,33 @@ class MedicinePackageAnalyzer:
         speech = ""  # Placeholder for generated speech
         return speech
 
+
+
+class BasicImageHandeling:
+
     @staticmethod
-    def save_output_images():
+    def read_image(image_path):
+        return cv2.imread(image_path)
+    
+    @staticmethod
+    def show_image(image):
+        plt.axis('off')
+        plt.imshow(image)
+
+    @staticmethod
+    def crop_image(image,coords):
+        x, y, w, h = coords
+        return image[y:y+h, x:x+w]
+    
+    @staticmethod
+    def save_output_images(): # TODO
         # Save o
         # utput images and other results
         pass
+
+    @staticmethod
+    def get_main_color_from_image(image): # TODO
+        return (255,255,255)
 
     @staticmethod
     def divide_rois(image, roi_coordinates):
@@ -68,8 +86,7 @@ class MedicinePackageAnalyzer:
         roi_imgs = {}
         # Iterate through each ROI
         for roi_name, (x, y, w, h) in roi_coordinates.items():
-            roi_imgs[roi_name] = MedicinePackageAnalyzer.crop_image(image_to_crop,(x,y,w,h))
-
+            roi_imgs[roi_name] = BasicImageHandeling.crop_image(image_to_crop,(x,y,w,h))
         return roi_imgs
 
     @staticmethod
